32b2bbde6e3cc41fb76598b5e3e4d8e2c498bf52
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-07 14:46:34 -0300

    feat(src/App.js): implementa lógica para catálogo de vinhos

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WineCatalog from './components/WineCatalog';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <WineCatalog />
+    </div>
+  );
+};
+
+export default App;

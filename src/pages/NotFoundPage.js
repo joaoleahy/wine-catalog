d7159b2639a0fa552f63bbb54b7f79ad576c99cd
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-12 10:18:27 -0300

    feat(pages/NotFoundPage): adiciona página de erro 404 (não encontrado)

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/NotFoundPage.css';
+
+const NotFoundPage = () => {
+  return (
+    <div className="not-found">
+      <h2>404 - Página não encontrada</h2>
+      <p>A página que você está procurando pode ter sido removida ou não está disponível temporariamente.</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;

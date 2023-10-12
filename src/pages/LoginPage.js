2b9582b99e38e3c804dad5014e0a4ac6c3d944ee
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-12 10:18:01 -0300

    feat(pages/LoginPage): adiciona página de login

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import '../styles/LoginPage.css';
+
+const LoginPage = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Dados de login:', formData);
+  };
+
+  return (
+    <div className="login-form">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Nome de Usuário ou E-mail:</label>
+          <input
+            type="text"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <label>Senha:</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;

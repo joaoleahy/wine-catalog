0dd786101562a489988990829e9acb672673f5ba
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-12 10:17:50 -0300

    feat(pages/AboutPage): adiciona página 'Sobre Nós'

---
+++ src/pages/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/AboutPage.css';
+
+const AboutPage = () => {
+  return (
+    <div className="about">
+      <h2>Sobre Nós</h2>
+      <p>Adicione aqui informações sobre a sua loja de vinhos ou qualquer outra informação relevante.</p>
+    </div>
+  );
+};
+
+export default AboutPage;

bb4d5a9e6519c5b563a23e0ed7239766736f0912
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-12 10:01:40 -0300

    refactor(components/WineCatalog): retoma descrição dos vinhos

--- src/components/WineCatalog.js
+++ src/components/WineCatalog.js
@@ -7,9 +7,33 @@ import roseWineImage from '../images/rose-wine.png';
 import '../styles/WineCatalog.css';
 
 const winesData = [
-  { id: 1, name: 'Reservado', type: 'Tinto', price: 40, image: redWineImage },
-  { id: 2, name: 'Naturelle', type: 'Rosé', price: 45, image: roseWineImage },
-  { id: 3, name: 'Freixenet', type: 'Branco', price: 60, image: whiteWineImage },
+  {
+    id: 1,
+    name: 'Reservado',
+    type: 'Tinto',
+    price: 40,
+    image: redWineImage,
+    description:
+      'Um vinho tinto encorpado com notas de frutas escuras e especiarias. Perfeito para acompanhar carnes vermelhas e queijos maturados.',
+  },
+  {
+    id: 2,
+    name: 'Naturelle',
+    type: 'Rosé',
+    price: 45,
+    image: roseWineImage,
+    description:
+      'Um vinho rosé delicadamente frutado com aromas de morango e notas florais. Ideal para momentos descontraídos e pratos leves.',
+  },
+  {
+    id: 3,
+    name: 'Freixenet',
+    type: 'Branco',
+    price: 60,
+    image: whiteWineImage,
+    description:
+      'Um vinho branco fresco e cítrico com toques de frutas tropicais. Combina perfeitamente com frutos do mar e saladas.',
+  },
 ];
 
 const WineCatalog = () => {

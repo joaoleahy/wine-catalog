e9448619e10872a8f8abf36201a616d08db78951
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-12 19:42:30 -0300

    feat(App): implementa rotas na aplicação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import WineCatalog from './components/WineCatalog';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes/Routes';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      <WineCatalog />
+      <Router>
+        <Routes />
+      </Router>
     </div>
   );
 };

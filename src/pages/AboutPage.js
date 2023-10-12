5ce52f50531dfc09e96c1e9a39ceb959de88211c
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-12 19:48:10 -0300

    refactor(pages/AboutPage): ajusta as sessões 'sobre nós'

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -4,8 +4,15 @@ import '../styles/AboutPage.css';
 const AboutPage = () => {
   return (
     <div className="about">
-      <h2>Sobre Nós</h2>
-      <p>Adicione aqui informações sobre a sua loja de vinhos ou qualquer outra informação relevante.</p>
+      <h2>Bem-vindo à Vinum Aeternum</h2>
+      <div className="about-content">
+        <p>Somos mais do que uma loja de vinhos, somos apaixonados pela arte da vinicultura, pela riqueza de sabores e pelo encanto que cada garrafa carrega consigo. Fundada por enófilos ávidos e conhecedores, nossa loja nasceu do amor profundo pela tradição, pela qualidade e pela história que cada vinho conta.</p>
+        <p><strong>Nossa Missão:</strong> Proporcionar a você uma experiência inigualável no universo dos vinhos. Nosso objetivo é conectar apreciadores e novatos com vinhos excepcionais de todas as partes do mundo, transformando cada gole em uma jornada fascinante.</p>
+        <p><strong>Nossa Seleção:</strong> Meticulosamente selecionada, nossa coleção representa os melhores produtores e as vinícolas mais renomadas de diversas regiões vinícolas. Cada garrafa em nossa prateleira é escolhida a dedo para garantir qualidade, autenticidade e sabor inigualável.</p>
+        <p><strong>Experiências Memoráveis:</strong> Além de nossa vasta seleção, organizamos regularmente eventos exclusivos e degustações que oferecem uma oportunidade única de conhecer produtores, explorar novos rótulos e mergulhar nas histórias por trás de cada safra.</p>
+        <p><strong>Sustentabilidade:</strong> Valorizamos o nosso planeta e trabalhamos com vinícolas que adotam práticas agrícolas sustentáveis, respeitando a terra e preservando a biodiversidade. Estamos comprometidos em oferecer opções ecologicamente responsáveis para sua tranquilidade e apreciação.</p>
+        <p>Estendemos um convite caloroso a todos os amantes do vinho, curiosos e exploradores aventureiros para nos visitarem. Junte-se a nós nesta jornada inesquecível. Na Vinum Aeternum, o vinho não é apenas uma bebida; é uma história esperando para ser contada. <strong>Cheers! 🍷</strong></p>
+      </div>
     </div>
   );
 };

50d010f3d42aafa83cbad964004b2b69ef229db7
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-16 17:44:57 -0300

    feat(CartContext): adiciona novo context para carrinho de compras

---
+++ src/contexts/CartContext.js
@@ -0,0 +1,29 @@
+import React, { createContext, useContext, useState } from 'react';
+
+export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (item) => {
+    setCartItems([...cartItems, item]);
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems(cartItems.filter(item => item.id !== itemId));
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (!context) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};

ed176da80e284c6dc9591009f1150c7813bca4fd
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-21 13:19:07 -0300

    fix(Server): ajusta conexão com o mongoDB

--- backend/src/Server.js
+++ backend/src/Server.js
@@ -6,13 +6,26 @@ const wineRoutes = require('./routes/WineRoutes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/vinhos', {
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => {
+    console.log('Connected to MongoDB!');
+    app.listen(process.env.PORT || 3001, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.use(cors());
 
@@ -22,8 +35,3 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/users', userRoutes);
 app.use('/wines', wineRoutes);
-
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Servidor iniciado na porta ${PORT}`);
-});

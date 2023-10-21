b2e5fc961ca859482cdc6325ac465d93bbb48aaf
João Leahy <joaovictorleahy.hrzon@gmail.com>
2023-10-21 11:18:48 -0300

    feat(UserController): implementa controller dos usuários

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,35 @@
+const User = require('../models/User');
+
+const userController = {
+  register: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const newUser = new User({ username, password });
+      await newUser.save();
+      res.status(201).json({ message: 'Usuário registrado com sucesso.' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  login: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+      if (!user) {
+        return res.status(401).json({ message: 'Usuário não encontrado.' });
+      }
+      const isPasswordValid = await user.comparePassword(password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Senha inválida.' });
+      }
+
+      res.status(200).json({ message: 'Login bem-sucedido.' });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+};
+
+module.exports = userController;
